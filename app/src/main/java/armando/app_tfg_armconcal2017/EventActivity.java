3207bd4097a839565d735fd485e5f6982ce799fb
Armando Conejero <dtcdc@hotmail.com>
2017-05-22 01:13:30 +0200

    Functionalities of event display added
    now you can join an event

--- app/src/main/java/armando/app_tfg_armconcal2017/EventActivity.java
+++ app/src/main/java/armando/app_tfg_armconcal2017/EventActivity.java
@@ -1,11 +1,15 @@
 package armando.app_tfg_armconcal2017;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -32,11 +36,14 @@ import java.util.ArrayList;
 public class EventActivity extends Activity {
     String idEvent = "";
     String user = "";
+    Button join;
     TextView name, description, restaurant, date, price, assistants;
     HttpClient httpclient = new DefaultHttpClient();
     ArrayList<NameValuePair> nameValuePairs;
     HttpPost httppost;
     ArrayList<Object> info = new ArrayList<>();
+    ArrayList<Object> number = new ArrayList<>();
+    ArrayList<Object> joinChecked = new ArrayList<>();
     Activity ctx = this;
 
     @Override
@@ -58,9 +65,10 @@ public class EventActivity extends Activity {
         date = (TextView) findViewById(R.id.txtEventDate);
         price = (TextView) findViewById(R.id.txtEventPrice);
         assistants = (TextView) findViewById(R.id.txtEventAssistants);
+        join = (Button) findViewById(R.id.btnEventJoin);
 
         new EventActivity.Info(EventActivity.this).execute();
-
+        new EventActivity.CheckAssistance(EventActivity.this).execute();
 
 
 
@@ -145,10 +153,285 @@ public class EventActivity extends Activity {
 
 
 
+    public String checkjoin() {
+        httppost = new HttpPost("http://armconcaltfg.esy.es/php/checkAssistance.php");
+        nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("idE", idEvent));
+        nameValuePairs.add(new BasicNameValuePair("user", user));
+        HttpResponse response;
+        String result = "";
+
+        try {
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream instream = entity.getContent();
+            result = convertStreamToString(instream);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private boolean joinStatus(){
+        String data = checkjoin();
+        System.out.println("Returns: " + data);
+        System.out.println(user);
+        if(!data.equals("[]\n")){
+            JSONObject json;
+            try{
+                json = new JSONObject(data);
+                JSONArray jsonArray = json.optJSONArray("info");
+                for(int i = 0; i < jsonArray.length(); i++){
+                    JSONObject jsonArrayChild = jsonArray.getJSONObject(i);
+                    joinChecked.add(jsonArrayChild.optString("user"));
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public class CheckAssistance extends AsyncTask<String, Float, String> {
+
+        private Activity ctx;
+
+        CheckAssistance(Activity ctx){
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(joinStatus()){
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String userId = joinChecked.get(0).toString();
+                        if(userId.equals(user)){
+                            join.setText(R.string.unjoin);
+                            join.setOnClickListener(new View.OnClickListener(){
+
+                                @Override
+                                public void onClick(View v) {
+                                    new DeleteAssistant(EventActivity.this).execute();
+                                }
+                            });
+
+                        }
+                    }
+                });
+            }else{
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        join.setText(R.string.join);
+                        join.setOnClickListener(new View.OnClickListener(){
+
+                            @Override
+                            public void onClick(View v) {
+                                new CreateAssistant(EventActivity.this).execute();
+                            }
+                        });
+                    }
+                });
+            }
+            return null;
+        }
+    }
 
 
+    public boolean assistance(){
+
+        httppost = new HttpPost("http://armconcaltfg.esy.es/php/createAssistant.php");
+        nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("idE", idEvent));
+        nameValuePairs.add(new BasicNameValuePair("user", user));
+
+        try{
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            httpclient.execute(httppost);
+            return true;
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }catch (ClientProtocolException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public class CreateAssistant extends AsyncTask<String, String, String> {
+
+        private Activity ctx;
+
+        CreateAssistant(Activity ctx){
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(assistance()){
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ctx, "you just Join the event", Toast.LENGTH_LONG).show();
+                        new UpdateNumber(EventActivity.this).execute();
+                        new CheckAssistance(EventActivity.this).execute();
+                        join.setOnClickListener(new View.OnClickListener(){
+
+                            @Override
+                            public void onClick(View v) {
+                                new DeleteAssistant(EventActivity.this).execute();
+                            }
+                        });
+                    }
+                });
+            }else{
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ctx, "Something went wrong", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
+    }
 
 
+    public boolean unassistance(){
+
+        httppost = new HttpPost("http://armconcaltfg.esy.es/php/deleteAssistant.php");
+        nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("idE", idEvent));
+        nameValuePairs.add(new BasicNameValuePair("user", user));
+
+        try{
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            httpclient.execute(httppost);
+            return true;
+        }catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }catch (ClientProtocolException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public class DeleteAssistant extends AsyncTask<String, String, String> {
+
+        private Activity ctx;
+
+        DeleteAssistant(Activity ctx){
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(unassistance()){
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ctx, "you just uncoil the event", Toast.LENGTH_LONG).show();
+                        new UpdateNumber(EventActivity.this).execute();
+                        new CheckAssistance(EventActivity.this).execute();
+                        join.setOnClickListener(new View.OnClickListener(){
+
+                            @Override
+                            public void onClick(View v) {
+                                new CreateAssistant(EventActivity.this).execute();
+                            }
+                        });
+                    }
+                });
+            }else{
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ctx, "Something went wrong", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
+    }
+
+
+    public String number() {
+        httppost = new HttpPost("http://armconcaltfg.esy.es/php/getEvent.php");
+        nameValuePairs = new ArrayList<NameValuePair>(1);
+        nameValuePairs.add(new BasicNameValuePair("id", idEvent));
+        HttpResponse response;
+        String result = "";
+
+        try {
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            InputStream instream = entity.getContent();
+            result = convertStreamToString(instream);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    private boolean filterNumber(){
+        String data = number();
+        System.out.println("Returns: " + data);
+        if(!data.equalsIgnoreCase("")){
+            JSONObject json;
+            try{
+                json = new JSONObject(data);
+                JSONArray jsonArray = json.optJSONArray("info");
+                for(int i = 0; i < jsonArray.length(); i++){
+                    JSONObject jsonArrayChild = jsonArray.getJSONObject(i);
+                    number.add(jsonArrayChild.optString("assistants"));
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public class UpdateNumber extends AsyncTask<String, Float, String> {
+
+        private Activity ctx;
+
+        UpdateNumber(Activity ctx){
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(filterNumber()){
+                ctx.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        assistants.setText(number.get(0).toString());
+                        number.clear();
+                    }
+                });
+            }
+            return null;
+        }
+    }
 
 
 
@@ -170,6 +453,39 @@ public class EventActivity extends Activity {
         }
     }
 
+
+    public class Back extends AsyncTask<String, Float, String> {
+
+        private Activity ctx;
+
+
+        Back(Activity ctx){
+            this.ctx = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            ctx.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(ctx, EventsActivity.class);
+                    Bundle b = getIntent().getExtras();
+                    b.putString("Id", idEvent);
+                    b.putString("User", user);
+                    intent.putExtras(b);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+            return null;
+        }
+    }
+
+
+    public void onBackPressed() {
+        new EventActivity.Back(EventActivity.this).execute();
+    }
 
 
 
